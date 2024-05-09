d6e9975fbad1643b2060871846bebe16b023931d
Jason Fuchs <jasonfuchs@protonmail.com>
2024-05-09 19:46:41 +0200

    Reworked PI constructor

--- src/pi.rs
+++ src/pi.rs
@@ -8,24 +8,24 @@ use pigpiod_if2::*;
 pub struct Pi(c_int);
 
 impl Pi {
-    pub fn new<A, P>(addr_str: A, port_str: P) -> Result<Self>
+    pub fn new<A, P>(addr_str: A, port_str: P) -> Result<Pi>
     where
         A: ToString,
         P: ToString,
     {
-        fn _new(addr_str: String, port_str: String) -> Result<Pi> {
-            let addr_str = CString::new(addr_str).map_err(|nul_err| Error::other(nul_err))?;
-            let port_str = CString::new(port_str).map_err(|nul_err| Error::other(nul_err))?;
-
+        fn _new(addr_str: CString, port_str: CString) -> Result<Pi> {
             let pi = unsafe { pigpio_start(addr_str.as_ptr(), port_str.as_ptr()) };
 
-            if pi.is_negative() {
-                return Err(Error::Pi(pi));
+            if pi < 0 {
+                Err(Error::Pi(pi))
+            } else {
+                Ok(Pi(pi))
             }
-
-            Ok(Pi(pi))
         }
 
-        _new(addr_str.to_string(), port_str.to_string())
+        _new(
+            CString::new(addr_str.to_string()).map_err(|e| Error::other(e))?,
+            CString::new(port_str.to_string()).map_err(|e| Error::other(e))?,
+        )
     }
 }
