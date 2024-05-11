d0966818a239ee78c323f453a9cf7c30e2e8b5d6
Jason Fuchs <jasonfuchs@protonmail.com>
2024-05-11 13:39:52 +0200

    Added wrap trait

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use anyhow::Result;
-use cps::prelude::*;
+use cps::{prelude::*, wrapper::Wrap};
 
 fn main() -> Result<()> {
     let _pi = Pi::new("rpi07")?;
+
+    let _ = 0.wrap();
 
     Ok(())
 }
--- src/wrapper.rs
+++ src/wrapper.rs
@@ -5,6 +5,16 @@ use std::ffi::CStr;
 // Using the newtype pattern
 pub struct W<T>(pub T);
 
+pub trait Wrap: Sized {
+    fn wrap(self) -> W<Self>;
+}
+
+impl<T: Sized> Wrap for T {
+    fn wrap(self) -> W<Self> {
+        W(self)
+    }
+}
+
 impl std::fmt::Display for W<&CStr> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.0.to_bytes().escape_ascii())
