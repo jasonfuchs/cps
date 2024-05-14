9266b9a1b846a913b459c2f48c7366f657260d56
Jason Fuchs <jasonfuchs@protonmail.com>
2024-05-14 11:02:35 +0200

    Added endless loop for displaing

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,29 @@ use anyhow::Result;
 use cps::prelude::*;
 
 fn main() -> Result<()> {
-    let _sh_reg = ShiftRegister::<4>::builder()
+    let sh_reg = ShiftRegister::<4>::builder()
         .addr("rpi07")
         .ds(17)
         .sh_cp(22)
         .st_cp(27)
         .build()?;
 
-    Ok(())
+    loop {
+        let file = sh_reg.get_ref().file_open(
+            "/sys/bus/w1/devices/10-00080253aa82/temperature",
+            pigpiod_if2::PI_FILE_READ,
+        )?;
+
+        let content: String = file.read::<16>()?.chars().filter(|&c| c != '\n').collect();
+
+        let milis: f32 = content.parse()?;
+
+        let temp = milis / 1000.0;
+
+        let before_dot = temp.to_string().chars().take_while(|&c| c != '.').count();
+
+        sh_reg.display(format!("{:.width$}", temp, width = 4 - before_dot))?;
+
+        std::mem::drop(file);
+    }
 }
