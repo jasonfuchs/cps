4ba9d73f0b153661235ae8f863d33d144bfcd48f
Jason Fuchs <jasonfuchs@protonmail.com>
2024-05-18 17:13:59 +0200

    Added display trait to tempreture

--- src/models.rs
+++ src/models.rs
@@ -1,16 +1,9 @@
+use std::fmt::{Display, Formatter};
+
 use chrono::prelude::*;
 use diesel::prelude::*;
 
 use crate::schema::temperatures;
-
-#[derive(Queryable, Selectable, Debug, PartialEq, PartialOrd)]
-#[diesel(table_name = temperatures)]
-#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
-pub struct Temperature {
-    id: i32,
-    temperature: f32,
-    created_at: NaiveDateTime,
-}
 
 #[derive(Insertable, Debug, PartialEq, PartialOrd)]
 #[diesel(table_name = temperatures)]
@@ -22,5 +15,20 @@ pub struct NewTemperature {
 impl From<f32> for NewTemperature {
     fn from(value: f32) -> Self {
         Self { temperature: value }
+    }
+}
+
+#[derive(Queryable, Selectable, Debug, PartialEq, PartialOrd)]
+#[diesel(table_name = temperatures)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
+pub struct Temperature {
+    id: i32,
+    temperature: f32,
+    created_at: NaiveDateTime,
+}
+
+impl Display for Temperature {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
+        write!(f, "|{}|{}|{}|", self.id, self.temperature, self.created_at)
     }
 }
