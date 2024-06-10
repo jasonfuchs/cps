e53149b6ed902fe580c65774f17ed6e5986be60b
Jason Fuchs <jasonfuchs@protonmail.com>
2024-06-10 12:49:17 +0200

    updated the wrapper

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -1,7 +1,15 @@
 use std::{ffi::CStr, fmt};
 
-// The all important wrapper
+/// The all important wrapper
+///
+/// Using new type pattern to add methods and traits to foriegn types
 pub struct W<T>(pub T);
+
+impl<T> From<T> for W<T> {
+    fn from(value: T) -> Self {
+        Self(value)
+    }
+}
 
 pub trait Wrap: Sized {
     fn wrap(self) -> W<Self>;
