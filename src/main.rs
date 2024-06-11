a14da73b428e004886300c4f97ba2434d0500258
Jason Fuchs <jasonfuchs@protonmail.com>
2024-06-11 19:20:32 +0200

    Refactored main file and cli

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,34 @@
-use std::{
-    ffi::c_uint,
-    num::NonZeroU32,
-    path::{Path, PathBuf},
-};
+use std::path::PathBuf;
 
+use clap::Parser;
+use cli::{Args, Format};
 use cps::prelude::*;
 use diesel::prelude::*;
 
-#[derive(clap::Parser, Debug)]
-struct Args {
-    #[arg(name = "ADDRESS")]
-    addr: String,
-    #[arg(short = 'p', long = "port", name = "PORT")]
-    port: Option<u16>,
-    #[arg(short = 'i', long = "input", name = "INPUT_PIN")]
-    ds: Option<c_uint>,
-    #[arg(short = 's', long = "shift", name = "SHIFT_PIN")]
-    sh_cp: Option<c_uint>,
-    #[arg(short = 'l', long = "latch", name = "LATCH_PIN")]
-    st_cp: Option<c_uint>,
-    #[arg(short = 'u', long = "url", name = "DATABASE_URL")]
-    db: Option<String>,
-    #[arg(short = 'd', long = "device", name = "DEVICE")]
-    dev: Option<String>,
-    #[arg(short = 'c', long = "count", name = "COUNT")]
-    count: Option<NonZeroU32>,
-    #[arg(short = 'f', long = "format", name = "FORMAT", value_enum)]
-    format: Option<Format>,
-}
-
-#[derive(clap::ValueEnum, Debug, Clone, Copy)]
-enum Format {
-    #[value(name = "txt")]
-    PlainText,
-    #[value(name = "csv")]
-    CommaSeperatedValues,
-}
+mod cli;
 
 fn main() -> anyhow::Result<()> {
-    let args = <Args as clap::Parser>::parse();
+    let args = Args::parse();
 
     let sh_reg = ShiftRegister::<4>::builder()
-        .addr(&args.addr)
-        .port(args.port.unwrap_or(8888))
-        .ds(args.ds.unwrap_or(17))
-        .sh_cp(args.sh_cp.unwrap_or(22))
-        .st_cp(args.st_cp.unwrap_or(27))
+        .addr(&args.address)
+        .port(args.port)
+        .ds(args.input)
+        .sh_cp(args.shift)
+        .st_cp(args.latch)
         .build()?;
 
-    let mut conn = SqliteConnection::establish(args.db.as_deref().unwrap_or("./diesel.db"))?;
+    let mut conn = SqliteConnection::establish(&args.url)?;
 
     let path = PathBuf::from("/sys/bus/w1/devices")
-        .join(args.dev.as_deref().unwrap_or("10-00080253aa82"))
+        .join(&args.device)
         .join("temperature");
 
-    #[inline]
-    fn _loop<const N: usize>(
-        args: &Args,
-        sh_reg: &ShiftRegister<N>,
-        conn: &mut SqliteConnection,
-        path: &Path,
-    ) -> anyhow::Result<()> {
+    let mut i = 0;
+    while args.count.map(u32::from).map_or(true, |count| i < count) {
         let file = sh_reg
             .get_ref()
-            .file_open(path, pigpiod_if2::PI_FILE_READ)?;
+            .file_open(&path, pigpiod_if2::PI_FILE_READ)?;
 
         let temp = file
             .read::<16>()?
@@ -71,38 +36,28 @@ fn main() -> anyhow::Result<()> {
             .take_while(|&c| c != '\n')
             .collect::<String>()
             .parse::<f32>()?
-            / 1000.0;
+            / 1000.;
 
-        let width = N - temp.to_string().chars().take_while(|&c| c != '.').count();
+        let width = 4 - temp.to_string().chars().take_while(|&c| c != '.').count();
 
-        sh_reg.display(format!("{:.width$}", temp, width = width))?;
+        sh_reg.display(format!("{temp:.width$}"))?;
 
         let row = {
-            use cps::schema::temperatures::dsl::*;
+            use cps::schema::temperatures::dsl::temperatures;
 
             diesel::insert_into(temperatures)
                 .values(NewTemperature::from(temp))
                 .returning(Temperature::as_returning())
-                .get_result(conn)?
+                .get_result(&mut conn)?
         };
 
-        match args.format.unwrap_or(Format::PlainText) {
-            Format::PlainText => println!("{}", row),
+        match args.format {
+            Format::PlainText => println!("{row}"),
             Format::CommaSeperatedValues => println!("{}", row.to_csv()),
         }
 
-        Ok(())
+        i += 1;
     }
 
-    match args.count {
-        None => loop {
-            _loop(&args, &sh_reg, &mut conn, &path)?;
-        },
-        Some(count) => {
-            for _ in 0..count.into() {
-                _loop(&args, &sh_reg, &mut conn, &path)?
-            }
-            Ok(())
-        }
-    }
+    Ok(())
 }
