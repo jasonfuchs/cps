9e1918abe3b8fbda9769d728cca9e7cb304e02e3
Jason Fuchs <jasonfuchs@protonmail.com>
2024-06-25 11:48:22 +0200

    use try again method if fail method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use std::path::PathBuf;
-use std::thread;
-use std::time::Duration;
 
 use anyhow::Result;
 use clap::Parser;
@@ -10,7 +8,8 @@ use cps::{
     segment_display::SegmentDisplay,
     shift_register::ShiftRegister,
 };
-use diesel::{Connection, RunQueryDsl, SelectableHelper, SqliteConnection};
+use diesel::prelude::*;
+use model::NewTemperature;
 
 mod cli;
 mod model;
@@ -51,31 +50,7 @@ fn main() -> Result<()> {
 
         sh_reg.write(format!("{temperature:.width$}"))?;
 
-        let row = {
-            use diesel::result::DatabaseErrorKind;
-            use diesel::result::Error;
-            use model::*;
-            use schema::temperatures::dsl::temperatures;
-
-            fn insert_into(
-                new_temperature: NewTemperature,
-                conn: &mut SqliteConnection,
-            ) -> Result<Temperature, Error> {
-                Ok(diesel::insert_into(temperatures)
-                    .values(new_temperature)
-                    .returning(Temperature::as_returning())
-                    .get_result(conn)?)
-            }
-
-            match insert_into(NewTemperature::from(temperature), &mut conn) {
-                Ok(row) => row,
-                Err(Error::DatabaseError(DatabaseErrorKind::UniqueViolation, _)) => {
-                    thread::sleep(Duration::from_secs(1));
-                    insert_into(NewTemperature::from(temperature), &mut conn)?
-                }
-                Err(err) => Err(err)?,
-            }
-        };
+        let row = NewTemperature::from(temperature).try_insert(&mut conn)?;
 
         match args.format {
             Format::PlainText => println!("{row}"),
--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,8 @@
-use std::fmt;
+use std::{
+    fmt::{Display, Formatter, Result as FmtResult},
+    thread,
+    time::Duration,
+};
 
 use chrono::prelude::*;
 use diesel::prelude::*;
@@ -14,6 +18,33 @@ impl From<f32> for NewTemperature {
     #[inline]
     fn from(temperature: f32) -> Self {
         Self { temperature }
+    }
+}
+
+impl NewTemperature {
+    #[inline]
+    pub fn try_insert(self, conn: &mut SqliteConnection) -> QueryResult<Temperature> {
+        #[inline]
+        fn _try_now(
+            temperature: &NewTemperature,
+            conn: &mut SqliteConnection,
+        ) -> QueryResult<Temperature> {
+            diesel::insert_into(crate::schema::temperatures::table)
+                .values(temperature)
+                .returning(Temperature::as_returning())
+                .get_result(conn)
+        }
+
+        Ok(
+            match _try_now(&self, conn).map_err(|_| {
+                thread::sleep(Duration::from_secs(1));
+                _try_now(&self, conn)
+            }) {
+                Ok(row) => row,
+                Err(Ok(row)) => row,
+                Err(err) => err?,
+            },
+        )
     }
 }
 
@@ -37,8 +68,8 @@ impl Temperature {
     }
 }
 
-impl fmt::Display for Temperature {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl Display for Temperature {
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
         let Self {
             created_at,
             temperature,
